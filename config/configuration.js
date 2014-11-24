49f825fbcf8cc59283826aa7eed9e3f472c232f3
buathi_q <buathi_q@epitech.eu>
2014-11-24 15:48:21 +0100

    Update concurrency params

--- config/configuration.js
+++ config/configuration.js
@@ -24,6 +24,8 @@ if(node_env === "production") {
 module.exports = {
   env: node_env,
   port: process.env.PORT || default_port,
+
+  usersConcurrency: process.env.GCALENDAR_USERS_CONCURRENCY || 1,
   concurrency: process.env.GCALENDAR_CONCURRENCY || 1,
 
   mongoUrl: process.env.MONGO_URL || process.env.MONGOLAB_URI,
